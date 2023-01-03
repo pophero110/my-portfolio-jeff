e8e5ebf0947fa536d1d92dd5b2a1721bb7e89a3c
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-03 17:31:01 -0600

    project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,9 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Awesome Chicken ',
+    description: 'MVP Restaurant Online Ordering via Next.js + Prisma + Stripe.',
+    imgSrc: '/static/images/chicken.jpg',
+    href: 'https://github.com/pophero110/awesome-chicken-wings-ecommerce',
   },
 ]
 //TODO: add my projects
