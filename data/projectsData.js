9584e0ac2d59a969a64ed9d74d6ac70586572848
Jeff Ou <38047622+pophero110@users.noreply.github.com>
2023-01-03 18:18:09 -0600

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Awesome Chicken ',
-    description: 'MVP Restaurant Online Ordering via Next.js + Prisma + Stripe.',
+    description: 'MVP Restaurant Online Ordering via Next.js + Prisma + Stripe. Mobile First UI Design',
     imgSrc: '/static/images/chicken.jpg',
     href: 'https://github.com/pophero110/awesome-chicken-wings-ecommerce',
   },
