2d28ae2fe02c18b913f1c3bd9ecf738a56f363f3
Jeff Ou <38047622+pophero110@users.noreply.github.com>
2023-01-12 17:34:58 -0600

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'Awesome Chicken ',
     description: 'MVP Restaurant Online Ordering',
-    tool: 'Next.js + Prisma + NextAuth.js + Stripe',
+    tool: 'Next.js + Prisma + NextAuth.js + Stripe + Moblie First Design ',
     imgSrc: '/static/images/chicken.jpg',
     href: 'https://awesome-chicken-wings.herokuapp.com/',
     repo: 'https://github.com/pophero110/awesome-chicken-wings-ecommerce',
