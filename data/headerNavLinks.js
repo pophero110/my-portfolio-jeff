e59e02b5bf7ff9773906ab9c158ad4205fbf3b88
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-01-20 12:28:01 -0600

    change home page

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
+  { href: '/', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/', title: 'Projects' },
+  { href: '/project', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
