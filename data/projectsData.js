8aecfef5b2417ff54bbcf52157ccc60b35ccc469
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-02-12 13:55:35 -0600

    update project data

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,8 +4,25 @@ const projectsData = [
     description: 'MVP Restaurant Online Ordering',
     tool: 'Next.js + Prisma + NextAuth.js + Stripe + Moblie First Design ',
     imgSrc: '/static/images/chicken.jpg',
-    href: 'https://awesome-chicken-wings.herokuapp.com/',
+    href: 'https://awesome-chicken-wings-ecommerce-a3uom88g8-pophero110.vercel.app/',
     repo: 'https://github.com/pophero110/awesome-chicken-wings-ecommerce',
+  },
+  {
+    title: 'Letter.io',
+    description: 'Letter writing app',
+    tool: 'Nest.js + Prisma + NextAuth.js + MongoDb',
+    imgSrc: '/static/images/hero-background.png',
+    href: 'https://letterio-web.vercel.app/',
+    repo: 'https://github.com/pophero110/letterio-web',
+  },
+
+  {
+    title: 'Tracker.io - WIP',
+    description: 'Customizable tracker app for your daily life',
+    tool: 'React Native + React.js + Mongoose + Express.js + Node.js + MongoDB',
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '#',
+    repo: 'https://github.com/pophero110/Track-My-Life-Backend',
   },
 ]
 //TODO: add my projects
